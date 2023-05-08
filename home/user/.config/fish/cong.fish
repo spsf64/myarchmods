fbf7738298a6c2c2dc5cecba3ac04fc03bd83cab
Sergio Schneider <spsf64@gmail.com>
2023-05-08 13:32:15 -0300

    Create cong.fish

---
+++ home/user/.config/fish/cong.fish
@@ -0,0 +1,9 @@
+export PATH="$PATH:$HOME/.bin"
+neofetch 
+figlet "ArchLinux + FishShell"
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+#neofetch
+end
